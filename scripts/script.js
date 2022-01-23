3ef64d7623b4ad6263c51f43e674bbcc1c1c11f2
Dima Ovsenev <danya.old@yandex.ru>
2022-01-23 15:23:06 +0300

    All page

--- scripts/script.js
+++ scripts/script.js
@@ -1,36 +1,38 @@
 
-let buttonopener = document.querySelector('.profile__edit-button');
+let buttonOpener = document.querySelector('.profile__edit-button');
 // Находим форму в DOM
-let formElement = document.querySelector('.form');
-let buttonclose = document.querySelector('.form__close-button');
-
-buttonopener.onclick = function () {
-formElement.classList.add('form_opened');
-}
-  buttonclose.onclick = function () {
-    formElement.classList.remove('form_opened');
+let formSection = document.querySelector ('.form');
+let formElement = formSection.querySelector('.form__container');
+let buttonClose = formElement.querySelector('.form__close-button');
+let buttonSave = formElement.querySelector ('.form__submit-button');
+buttonOpener.addEventListener('click',function () {
+  formSection.classList.add('form_opened');
+  })
+  buttonClose.addEventListener ('click', close);
+  function close () {
+    formSection.classList.remove('form_opened');
   }
 // Воспользуйтесь методом querySelector()
 // Находим поля формы в DOM
-let nameInput = formElement.querySelector ('#name');// Воспользуйтесь инструментом .querySelector()
-let jobInput = formElement.querySelector ('#job');// Воспользуйтесь инструментом .querySelector()
+// let nameInput = formElement.querySelector ('#name');// Воспользуйтесь инструментом .querySelector()
+// let jobInput = formElement.querySelector ('#job');// Воспользуйтесь инструментом .querySelector()
 
+//let inputs = document.querySelectorAll('input');// Получите значение полей jobInput и nameInput из свойства value
+
+// Выберите элементы, куда должны быть вставлены значения полей
+let name = document.querySelector('.profile__name');
+let job = document.querySelector('.profile__describe');
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
 function formSubmitHandler (evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
                                                 // Так мы можем определить свою логику отправки.
                                                 // О том, как это делать, расскажем позже.
-
-    let inputs = document.querySelectorAll('input');// Получите значение полей jobInput и nameInput из свойства value
-
-    // Выберите элементы, куда должны быть вставлены значения полей
-    let profile = document.querySelector ('.profile');
-    let name = profile.querySelector('.profile__name');
-    let job = profile.querySelector ('.profile__describe');
     // Вставьте новые значения с помощью textContent
-    name.textContent = inputs[0].value;
-    job.textContent = inputs[1].value;
+    console.log(this.name);
+    name.textContent = this.name.value;
+    job.textContent = this.job.value;
+    close();
 }
 
 // Прикрепляем обработчик к форме:
