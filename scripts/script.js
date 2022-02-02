6bf1a0aa8a1cd3b8b6b0408741ac6f3193c990ca
Dima Ovsenev <danya.old@yandex.ru>
2022-02-02 17:34:58 +0300

    Auto_Add_Cards

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,42 @@
 
-let buttonOpener = document.querySelector('.profile__edit-button');
-// Находим форму в DOM
-let formSection = document.querySelector ('.form');
-let formElement = formSection.querySelector('.form__popup');
+let buttonOpener = document.querySelector('.profile__edit-button'); // Кнопка редактирования
+let formSection = document.querySelector ('.form'); //Форма
+let formElement = formSection.querySelector('.form__popup'); // Сам попап
 let formEdit = formElement.querySelector('.form__edit');
 let buttonClose = formElement.querySelector('.form__close-button');
-let nameInput = formElement.querySelector ('#name');
-let jobInput = formElement.querySelector ('#job');
+let nameInput = formElement.querySelector ('#name'); //Имя профиля в форме
+let jobInput = formElement.querySelector ('#job'); //Описание профиля  в форме
+let name = document.querySelector('.profile__name');  // Имя профиля в разметке
+let job = document.querySelector('.profile__describe'); // Описание профиля в разметке
+let galery = document.querySelector ('.places');
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
 buttonOpener.addEventListener('click',function () {
 
   formSection.classList.add('form_opened');
@@ -17,19 +47,11 @@ buttonOpener.addEventListener('click',function () {
   function close () {
     formSection.classList.remove('form_opened');
   }
-// Воспользуйтесь методом querySelector()
-// Находим поля формы в DOM
-// let nameInput = formElement.querySelector ('#name');// Воспользуйтесь инструментом .querySelector()
-// let jobInput = formElement.querySelector ('#job');// Воспользуйтесь инструментом .querySelector()
 
-//let inputs = document.querySelectorAll('input');// Получите значение полей jobInput и nameInput из свойства value
 
-// Выберите элементы, куда должны быть вставлены значения полей
-let name = document.querySelector('.profile__name');
-let job = document.querySelector('.profile__describe');
-
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
+initialCards.forEach (card=>{
+  galery.insertAdjacentHTML("afterbegin", `<article class = "places__card"><img class = "places__image" src=${card.link}><div class = "places__title"><h2 class = "places__text">${card.name}</h2><button type = "button" class = places__button aria-label ="Поставить лайк"></button></div></article>`)
+})
 function formSubmitHandler (evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
                                                 // Так мы можем определить свою логику отправки.
