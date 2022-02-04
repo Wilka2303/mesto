fee6c57b8e3c2f89d2ef84cd00b2a28946fc7554
Dima Ovsenev <danya.old@yandex.ru>
2022-02-04 15:59:30 +0300

    Add_Cards

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,22 @@
 
 let buttonEditOpener = document.querySelector('.profile__edit-button');
 let buttonAddOpener = document.querySelector('.profile__add-button'); // Кнопка редактирования
-let formSection = document.querySelector ('.form'); //Форма
+let formSection = document.querySelector('.form'); //Форма
 let formElement = formSection.querySelector('.form__popup'); // Сам попап
 let formEdit = formElement.querySelector('.form__edit');
-let nameInput = formElement.querySelector ('#name'); //Имя профиля в форме
-let jobInput = formElement.querySelector ('#job'); //Описание профиля  в форме
+let nameInput = formElement.querySelector('#name'); //Имя профиля в форме
+let jobInput = formElement.querySelector('#job'); //Описание профиля  в форме
 let name = document.querySelector('.profile__name');  // Имя профиля в разметке
 let job = document.querySelector('.profile__describe'); // Описание профиля в разметке
 let editForm = document.querySelector('#form_edit');
 let addForm = document.querySelector('#form_add');
 let buttonCloseEdit = editForm.querySelector('.form__close-button');
-let buttonCloseAdd  = addForm.querySelector('.form__close-button');
+let buttonCloseAdd = addForm.querySelector('.form__close-button');
+let templ = document.querySelector('#gallery_cards').content;
+const placesElement = document.querySelector('.places');
+let inputCard = this.name_card.value;
+let inputLink = this.link.value;
+
 const initialCards = [
   {
     name: 'Архыз',
@@ -44,59 +49,67 @@ function popupopenner(name_popup) {
 }
 
 
-function closer_popup (name_popup) {
+function closer_popup(name_popup) {
   name_popup.classList.remove('form_opened');
 }
 
-
-
-buttonEditOpener.addEventListener('click',function () {
+buttonEditOpener.addEventListener('click', function () {
   popupopenner(editForm);
-  nameInput.setAttribute('value',name.textContent);
-  jobInput.setAttribute('value',job.textContent);
-  })
-  buttonCloseEdit.addEventListener ('click', function() {
-    closer_popup(editForm);
-  });
+  nameInput.setAttribute('value', name.textContent);
+  jobInput.setAttribute('value', job.textContent);
+})
+buttonCloseEdit.addEventListener('click', function () {
+  closer_popup(editForm);
+});
 
-
-
-
-buttonAddOpener.addEventListener('click',function () {
+buttonAddOpener.addEventListener('click', function () {
   popupopenner(addForm);
-//  name_card.setAttribute('value',name.textContent);
-//  link.jobInput.setAttribute('value',job.textContent);
-  })
-  buttonCloseAdd.addEventListener ('click', function() {
-    closer_popup(addForm);
-  });
-  function gallery (){
+})
+buttonCloseAdd.addEventListener('click', function () {
+  closer_popup(addForm);
+});
 
-  let templ = document.querySelector('#gallery_cards').content;
-  for (let i = 0; i<initialCards.length; i++){
-    let a = document.querySelector ('.new_one');
-    let data = initialCards[i];
-    const copy = templ.querySelector('.places__card').cloneNode(true);
-    copy.querySelector('.places__text').textContent = data.name;
-    copy.querySelector('.places__image').src = data.link;
-    a.appendChild(copy)
+function gallery() {
+  placesElement.innerHTML = '';
+  for (let i = 0; i < initialCards.length; i++) {
+    //  let a = document.querySelector ('.new_one');
+    let data = initialCards[i]; //Объявляем массив
+    const copy = templ.querySelector('.places__card').cloneNode(true); //Клонируем содержимое
+    let image_places = copy.querySelector('.places__image'); //Выбираем элемент
+    let text_places = copy.querySelector('.places__text'); // Выбираем элемент
+    text_places.textContent = data.name; // Получем элемент из Массива c названием карточки
+    image_places.src = data.link; // Получаем элемент с названием каточки
+    placesElement.appendChild(copy);
   }
+
+}
+gallery();
+
+function inputes (inp1, inp2){
+  inp1.textContent = inp2.value;
 }
 
-gallery();
-/*initialCards.forEach (card=>{
-  galery.insertAdjacentHTML("afterbegin", `<article class = "places__card"><img class = "places__image" src=${card.link}><div class = "places__title"><h2 class = "places__text">${card.name}</h2><button type = "button" class = places__button aria-label ="Поставить лайк"></button></div></article>`)
-})*/
-function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                                                // Так мы можем определить свою логику отправки.
-                                                // О том, как это делать, расскажем позже.
-    // Вставьте новые значения с помощью textContent
-    name.textContent = this.name.value;
-    job.textContent = this.job.value;
-    close();
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  inputes(name,this.name);
+  inputes(job,this.job);
+  closer_popup(editForm);
+}
+
+function Construct(names, links) {
+  this.name = names;
+  this.link = links;
+}
+
+function formAddHandler(evt){
+  evt.preventDefault();
+ // console.log({name: this.name_card.value , link: this.link.value});
+  initialCards.unshift ({name: name_card.value, link: link.value});
+  console.log ({name: name_card.value, link: link.src})
+  gallery();
 }
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formEdit.addEventListener('submit', formSubmitHandler);
+addForm.addEventListener('submit', formAddHandler);
