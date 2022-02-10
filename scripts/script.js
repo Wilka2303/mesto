f70303c0512566565c7b04b6e5c83decbabc994f
Dima Ovsenev <danya.old@yandex.ru>
2022-02-10 10:41:54 +0300

    fix_massive

--- scripts/script.js
+++ scripts/script.js
@@ -17,32 +17,6 @@ const template = document.querySelector('#gallery_cards').content;
 const placesElement = document.querySelector('.places');
 const trashButton = document.querySelector('.places__trash');
 
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
 
 function openPopup(namePopup) {
   namePopup.classList.add('popup_opened');
